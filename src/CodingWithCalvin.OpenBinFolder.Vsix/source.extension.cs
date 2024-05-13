7d54d38051ecba8b0bef95185be1a988d5e1cbce
Calvin A. Allen <calvin.allen@outlook.com>
2024-05-13 12:59:39 -0400

    Update source.extension.cs
    [skip ci]

--- src/CodingWithCalvin.OpenBinFolder.Vsix/source.extension.cs
+++ src/CodingWithCalvin.OpenBinFolder.Vsix/source.extension.cs
@@ -7,7 +7,7 @@ namespace CodingWithCalvin.OpenBinFolder.Vsix
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "0F713362-1708-4956-85F7-A3C204735E3C";
+        public const string Id = "VS-OpenBinFolder";
         public const string Name = "Open Bin Folder";
         public const string Description = @"Adds an 'Open Bin Folder' to the right click menu of the project for quick access to the builds output folder.";
         public const string Language = "en-US";
